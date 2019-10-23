cfbdb36fd98ee052a5fd77027dbbb798afe88e1b
Not Important <56871876+AI-Programmer@users.noreply.github.com>
2019-10-23 11:08:54 +0700

    Add files via upload

---
+++ MuhammadAnangMaruf.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muhammadanangmaruf;
+
+/**
+ *
+ * @author DD
+ */
+public class MuhammadAnangMaruf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Long NIK = 351411240898l;
+       String Nama="MUHAMMAD ANANG MA'RUF";
+       String TTL="Surabaya,24-08-1998";
+       String JK="Laki-laki";
+       String Alamat="Dusun Suket";
+       String RTRW="001/011";
+       String KelDesa="Nogosari";
+       String Kecamatan="Pandaan";
+       String Agama="Islam";
+       String Statusperkawinan="Belum Menikah";
+       String Pekerjaan="Pelajar/Mahasiswa";
+       String Kewarganegaraan="WNI";
+       String Berlaku="Seumur Hidup";
+       String Jurusan="Teknik Informatika";
+       Long NIM=201969040019l;
+       int Angkatan=2019;
+       String UYP="Yudharta Pasuruan";
+       
+       System.out.println("NIK                  : "+NIK);
+       System.out.println("Nama                 : "+Nama);
+       System.out.println("Tempat,Tanggal Lahir : "+TTL);
+       System.out.println("Jenis Kelamin        : "+JK);
+       System.out.println("Alamat               : "+Alamat);
+       System.out.println("RT/RW                : "+RTRW);
+       System.out.println("Kel/Desa             : "+KelDesa);
+       System.out.println("Kecamatan            : "+Kecamatan);
+       System.out.println("Agama                : "+Agama);
+       System.out.println("Status Perkawianan   : "+Statusperkawinan);
+       System.out.println("Pekerjaan            : "+Pekerjaan);
+       System.out.println("Kewarganegaraan      : "+Kewarganegaraan);
+       System.out.println("Berlaku              : "+Berlaku);
+       System.out.println("Jurusan              : "+Jurusan);
+       System.out.println("NIM                  : "+NIM);
+       System.out.println("Angkatan             : "+Angkatan);
+       System.out.println("Universitas          : "+UYP);
+    }
+    
+}
